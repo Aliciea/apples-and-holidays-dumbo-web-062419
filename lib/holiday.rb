7a61fdbc55e402bf0ae939ad7ddfe2636aa7cde8
Aliciea D Suechin <adr330@nyu.edu>
2019-06-19 18:41:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,11 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
     holiday_hash = ["fourth_of_july", "memorial_day"]
+   holiday_hash.values 
     return holiday_hash
 end
+end 
+
 
 
 
